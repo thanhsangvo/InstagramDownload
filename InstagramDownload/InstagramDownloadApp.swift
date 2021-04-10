fb8587a2340a54c2c2f81b49f93845a9dacd4312
thanhsangvo <39442495+thanhsangvo@users.noreply.github.com>
2021-04-10 15:33:14 +0700

    Initial Commit

---
+++ InstagramDownload/InstagramDownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramDownloadApp.swift
+//  InstagramDownload
+//
+//  Created by Vo Thanh Sang on 10/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramDownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
